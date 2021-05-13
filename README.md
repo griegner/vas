6a58174681612bd24e50f56fe38f8216e9292362
griegner <gabriel.riegner@gmail.com>
2021-05-13 13:57:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## vas
+> visual analog scale controlled by keyboard or MRI-compatible botton box
+
+<img src='vas.gif' width='500'>
+
+**to run**:
+- `$ python vas.py [--screen <num>]`
+-  `--screen <num>` local (0) or projector (1)
+- responds to keys mapped to [ 1 ] and [ 2 ]
+- note: button box should not send release code until key is released
+
+**requirements**:
+- [psychopy](https://www.psychopy.org/download.html#conda)
+- psychtoolbox==3.0.16
---
+++ vas.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env conda run --name psychopy python
+
+from psychopy import visual, core, logging
+from psychopy.hardware import keyboard
+import argparse
+logging.console.setLevel(logging.CRITICAL)
+
+class Style:
+    bold = '\033[1m'
+    red = '\033[91m'
+    end = '\033[0m'
+
+def get_args():
+    parser = argparse.ArgumentParser(description='set screen number')
+    parser.add_argument('--screen', type=int, choices=[0,1], default=0, help='local (0) or remote (1)')
+    return parser.parse_args()
+
+def init_objects(x, y, screen):
+    kb = keyboard.Keyboard()
+    timer = core.CountdownTimer(10)
+    fullscr=False
+    if screen: fullscr=True
+    win = visual.Window(size=(x,y), pos=(0,0), color='white', allowGUI=True, fullscr=fullscr, screen=screen)
+    title = visual.TextStim(win=win, text=None, color='black', pos=[0,.25])
+    time = visual.TextStim(win=win, text='', color='red', pos=[0,-.25])
+    outline = visual.Rect(win=win, width=1, height=.2)
+    outline.lineColor = 'black'
+    outline.lineWidth = 2.3
+    rect = visual.Rect(win=win, width=.01, height=.2)
+    rect.fillColor = 'red'
+    pain0 = visual.TextStim(win=win, color='black', height=.055, pos=[-.7, 0])
+    pain10 = visual.TextStim(win=win, color='black', height=.055, pos=[.75, 0])
+    return kb, timer, win, title, time, outline, rect, pain0, pain10
+
+def reset(title, time, rect, pain_type, pain0, pain10):
+    title.text = pain_type
+    time.text = ''
+    rect.pos = [-.5,0]
+    rect.width = .01
+    rect.height = .2
+    if pain_type == 'PAIN INTENSITY':
+        pain0.text = 'NO PAIN\nSENSATION'
+        pain10.text = 'MOST INTENSE PAIN\nSENSATION IMAGINABLE'
+    elif pain_type == 'PAIN UNPLEASANTNESS':
+        pain0.text = 'NOT AT ALL\nUNPLEASANT'
+        pain10.text = 'MOST UNPLEASANT\nIMAGINABLE'
+
+def update_win(win, title, time, outline, rect, pain0, pain10):
+    rect.draw()
+    outline.draw()
+    title.draw()
+    time.draw()
+    pain0.draw()
+    pain10.draw()
+    win.flip()
+
+def main():
+
+    x = 2046
+    y = 1152
+
+    args = get_args()
+
+    kb, timer, win, title, time, outline, rect, pain0, pain10 = init_objects(x, y, args.screen)
+
+    for pain_type in ['PAIN INTENSITY', 'PAIN UNPLEASANTNESS']:
+
+        reset(title, time, rect, pain_type, pain0, pain10)
+        timer.reset()
+        pain_rating = 0
+        while timer.getTime() > 0:
+            update_win(win, title, time, outline, rect, pain0, pain10)
+            keys = kb.getKeys(['1', '2', 'return'], waitRelease=False, clear=False)
+
+            remaining = timer.getTime()
+            if remaining < 3: time.text = f'{remaining:.1f}'
+
+            if keys and not keys[-1].duration: # key is being held down
+                key = keys[-1].name
+                if key == '1' and pain_rating > 0:
+                    pain_rating -= .05
+                    rect.width -= .005
+                    rect.pos[0] -= .0025
+                elif key == '2' and pain_rating < 10:
+                    pain_rating += .05
+                    rect.width += .005
+                    rect.pos[0] += .0025
+                del keys
+
+        print(Style.bold+Style.red + f'{pain_type[5:8]}:\t{pain_rating:.2f}' + Style.end)
+        win.flip()
+        core.wait(1)
+
+if __name__ == '__main__': main()
